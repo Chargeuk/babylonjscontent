14d881a5c18d345adaf2039147198b1a9e2f4041
chargeuk <d_a_stapleton@hotmail.com>
2022-04-05 10:51:51 +0100

    updated test image files from kye for ios ktx2 orientation test

--- ktx2Test/convert-commands.bat
+++ ktx2Test/convert-commands.bat
@@ -2,3 +2,6 @@ toktx --genmipmap --bcmp --clevel 5 redTop.ktx2 redTop.png
 toktx --genmipmap --bcmp --clevel 5 greenTop.ktx2 greenTop.png
 toktx --genmipmap --bcmp --clevel 5 copper.ktx2 copper.png
 
+toktx --genmipmap --bcmp --clevel 5 red-top.ktx2 red-top.png
+toktx --genmipmap --bcmp --clevel 5 green-top-flip-manual.ktx2 green-top-flip-manual.png
+toktx --genmipmap --bcmp --clevel 5 green-top-flip-vertical.ktx2 green-top-flip-vertical.png
